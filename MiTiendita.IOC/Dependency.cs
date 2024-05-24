da945ebd853040c4052e3502f3d7ca568e46b793
alemndev <140274341+alemndev@users.noreply.github.com>
2024-05-24 01:13:26 -0500

    feat(api, ioc): implemented dependency injection

---
+++ MiTiendita.IOC/Dependency.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MiTiendita.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiTiendita.IOC
+{
+  public static class Dependency
+  {
+    public static void DependencyInjection(this IServiceCollection services, IConfiguration config)
+    {
+      services.AddDbContext<MitienditaDbContext>(opt =>
+      {
+        opt.UseSqlServer(config.GetConnectionString("sqlString"));
+      });
+    }
+  }
+}
