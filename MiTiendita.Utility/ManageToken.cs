1e9ae568c74de3f0e49cdc3a11697c894ca73374
alemndev <140274341+alemndev@users.noreply.github.com>
2024-05-25 10:20:38 -0500

    feat(utility, api): added Authentication

--- MiTiendita.Utility/ManageToken.cs
+++ MiTiendita.Utility/ManageToken.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MiTiendita.Entity;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +13,32 @@ namespace MiTiendita.Utility
 {
   public class ManageToken
   {
+    public string CreateToken(IConfiguration config, User model)
+    {
+      var key = config.GetSection("JWTSettings:key").Value;
+      var keyBytes = Encoding.ASCII.GetBytes(key);
+
+      var credentialsToken = new SigningCredentials(
+        new SymmetricSecurityKey(keyBytes),
+        SecurityAlgorithms.HmacSha256
+      );
+
+      var claims = new ClaimsIdentity();
+
+      claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, Convert.ToString(model.UserId)));
+      claims.AddClaim(new Claim(ClaimTypes.Role, Convert.ToString(model.UserRole)));
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = claims,
+        Expires = DateTime.UtcNow.AddDays(1),
+        SigningCredentials = credentialsToken
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+      
+      return tokenHandler.WriteToken(tokenConfig);
+    }
   }
 }
--- MiTienditaBackend.API/Program.cs
+++ MiTienditaBackend.API/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MiTiendita.IOC;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,28 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.DependencyInjection(builder.Configuration);
 
+var key = builder.Configuration.GetSection("JWTSettings:key").Value;
+var keyBytes = Encoding.ASCII.GetBytes(key);
+
+builder.Services.AddAuthentication(conf =>
+{
+  conf.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  conf.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(conf =>
+{
+  conf.RequireHttpsMetadata = false;
+  conf.SaveToken = true;
+  conf.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+    ValidateIssuer = false,
+    ValidateAudience = false,
+    ValidateLifetime = true,
+    ClockSkew = TimeSpan.Zero
+  };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +44,8 @@ if (app.Environment.IsDevelopment())
   app.UseSwagger();
   app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
